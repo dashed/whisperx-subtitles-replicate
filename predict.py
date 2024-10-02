d4c573e03f05fee848114f6028af4b69c0e6c5f5
Alberto Leal <mail4alberto@gmail.com>
2024-10-02 09:09:44 -0400

    fix

--- predict.py
+++ predict.py
@@ -483,13 +483,18 @@ def split_subtitle(text: str, max_chars=42) -> str:
     current_length = 0
 
     for word in words:
-        if current_length + len(word) + 1 > max_chars and current_line:
+        word_length = len(word)
+        if current_length + word_length + (1 if current_line else 0) > max_chars:
             lines.append(" ".join(current_line))
             current_line = [word]
-            current_length = len(word)
+            current_length = word_length
         else:
-            current_line.append(word)
-            current_length += len(word) + 1
+            if current_line:
+                current_line.append(word)
+                current_length += word_length + 1  # Account for space
+            else:
+                current_line.append(word)
+                current_length += word_length
 
     if current_line:
         lines.append(" ".join(current_line))
